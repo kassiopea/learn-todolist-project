0411c38c340669e0dd2c8c91d3bb08242af4c4d4
RandomNaNastya <randmonanastya@gmail.com>
2021-09-06 20:57:44 +0300

    create templates with def names

---
+++ tests/tests_api/tests/auth/logout.py
@@ -0,0 +1,20 @@
+class TestInvalidLogout:
+    def test_check_invalid_logout_without_cookie(self):
+        pass
+
+    def test_check_invalid_logout_without_xscrf_token(self):
+        pass
+
+    def test_check_invalid_logout_with_invalid_cookie(self):
+        pass
+
+    def test_check_invalid_logout_with_invalid_xscrf_token(self):
+        pass
+
+
+class TestValidLogout:
+    def test_check_logout(self):
+        pass
+
+    def test_check_expired_token(self):
+        pass
