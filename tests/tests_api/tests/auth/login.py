68c8677a84350fe8f4151b71375abbbdb25e774d
RandomNaNastya <randmonanastya@gmail.com>
2021-09-06 20:30:20 +0300

    доÐadd valid login api tests

--- tests/tests_api/tests/auth/login.py
+++ tests/tests_api/tests/auth/login.py
@@ -385,3 +385,146 @@ class TestLoginWithInvalidFields:
         assert response_body["data"] is None
         message = response_body["errors"]["login"]
         assert message == UserAuthErrors.LOGIN_DOES_NOT_EXIST
+
+    def test_check_invalid_username_and_pwd(
+            self,
+            base_url,
+            get_base_header,
+            get_registered_user
+    ):
+        auth = ApiAuth(base_url=base_url)
+        data = {
+            "username": get_registered_user["username"] + "I",
+            "password": get_registered_user["password"] + "TEST"
+        }
+
+        response = auth.login(
+            path=AuthUrls.LOGIN,
+            data=data,
+            headers=get_base_header
+        )
+
+        assert response.status_code == 400
+        response_body = response.json()
+        assert response_body["data"] is None
+        message = response_body["errors"]["login"]
+        assert message == UserAuthErrors.LOGIN_DOES_NOT_EXIST
+
+    def test_check_invalid_email_and_pwd(
+            self,
+            base_url,
+            get_base_header,
+            get_registered_user
+    ):
+        auth = ApiAuth(base_url=base_url)
+        data = {
+            "email": "test" + get_registered_user["email"],
+            "password": "test" + get_registered_user["password"]
+        }
+
+        response = auth.login(
+            path=AuthUrls.LOGIN,
+            data=data,
+            headers=get_base_header
+        )
+
+        assert response.status_code == 400
+        response_body = response.json()
+        assert response_body["data"] is None
+        message = response_body["errors"]["login"]
+        assert message == UserAuthErrors.LOGIN_DOES_NOT_EXIST
+
+
+class TestLoginWithValidFields:
+    def test_check_login_with_valid_username_and_pwd(
+            self,
+            base_url,
+            get_base_header,
+            get_registered_user
+    ):
+        auth = ApiAuth(base_url=base_url)
+        data = {
+            "username": get_registered_user["username"],
+            "password": get_registered_user["password"]
+        }
+        response = auth.login(
+            path=AuthUrls.LOGIN,
+            data=data,
+            headers=get_base_header
+        )
+
+        assert response.status_code == 200
+        response_body = response.json()
+        current_username = response_body["data"]["username"]
+        current_status = response_body["data"]["status"]
+        assert current_username == get_registered_user["username"]
+        assert current_status == "LoggedIn"
+
+    def test_check_login_with_valid_email_and_pwd(
+            self,
+            base_url,
+            get_base_header,
+            get_registered_user
+    ):
+        auth = ApiAuth(base_url=base_url)
+        data = {
+            "email": get_registered_user["email"],
+            "password": get_registered_user["password"]
+        }
+        response = auth.login(
+            path=AuthUrls.LOGIN,
+            data=data,
+            headers=get_base_header
+        )
+
+        assert response.status_code == 200
+        response_body = response.json()
+        current_username = response_body["data"]["email"]
+        current_status = response_body["data"]["status"]
+        assert current_username == get_registered_user["email"]
+        assert current_status == "LoggedIn"
+
+    def test_check_tokens_after_login_with_valid_username_and_pwd(
+            self,
+            base_url,
+            get_base_header,
+            get_registered_user
+    ):
+        auth = ApiAuth(base_url=base_url)
+        data = {
+            "username": get_registered_user["username"],
+            "password": get_registered_user["password"]
+        }
+        response = auth.login(
+            path=AuthUrls.LOGIN,
+            data=data,
+            headers=get_base_header
+        )
+
+        assert response.status_code == 200
+        assert "Set-Cookie" in response.headers
+        cookies = response.cookies
+        assert "access_token", "csrf_access_token" in cookies
+
+    def test_check_tokens_after_login_with_valid_email_and_pwd(
+            self,
+            base_url,
+            get_base_header,
+            get_registered_user
+    ):
+        auth = ApiAuth(base_url=base_url)
+        data = {
+            "email": get_registered_user["email"],
+            "password": get_registered_user["password"]
+        }
+        response = auth.login(
+            path=AuthUrls.LOGIN,
+            data=data,
+            headers=get_base_header
+        )
+
+        assert response.status_code == 200
+        assert "Set-Cookie" in response.headers
+        cookies = response.cookies
+        assert "access_token", "csrf_access_token" in cookies
+
