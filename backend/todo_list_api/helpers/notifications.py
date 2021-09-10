1cb1ee895ffb14a91cf4ea7ed7db764c8d7dd8a1
randomnanastya <randomnanastya@gmail.com>
2021-09-10 09:31:44 +0300

    добавилав notifications функцию создания одного сообщения. Возвращает словарь, вместо списка

--- backend/todo_list_api/helpers/notifications.py
+++ backend/todo_list_api/helpers/notifications.py
@@ -2,9 +2,14 @@ class Notifications:
     def __init__(self, result_type):
         self.result_type = result_type
         self.result = {self.result_type: []}
+        self.result_notification = {self.result_type: {}}
 
     def add_notification(self, field, message):
         self.result[self.result_type].append({field: message})
+
+    def create_notification(self, field, message):
+        self.result_notification[self.result_type][field] = message
+        return self.result_notification
 
     def get_notifications(self):
         return self.result
